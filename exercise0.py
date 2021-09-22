6707d2081cfdb6a14cd7a3f06d172684b9e353c6
Luke Reynolds <31745084+lukereynoldspi@users.noreply.github.com>
2021-09-22 16:49:47 -0700

    test

---
+++ exercise0.py
@@ -0,0 +1,12 @@
+print("2021 F1 Drivers")
+print("===============")
+print("")
+
+driversdict = {
+    "firstname": "Max",
+    "lastname": "Verstappen",
+    "number": 33,
+    "brand": "Red Bull"
+}
+
+print(driversdict)
